449dc74b31b4e16e20929072c71b4e561a7cf6c6
choijaeyeong <sylove532@gmail.com>
2020-10-30 14:03:25 +0900

    디비연결

--- mini/src/main/java/co/mini/dao/DAO.java
+++ mini/src/main/java/co/mini/dao/DAO.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class DAO {
 	
 	private String driver = "oracle.jdbc.driver.OracleDriver";
-	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	private String user = "choi";
+	private String url = "jdbc:oracle:thin:@192.168.0.116:1521:xe";
+	private String user = "black";
 	private String pw = "1234";
 	
 	public Connection conn; //DB 연결 객체	
