ec5a328dc9005c8444d0d0b5ccd3dd53ee1c3224
haez <hae_z@kakao.com>
2020-11-13 08:55:53 +0900

    1113 컨트롤러 수정 해주

---
+++ mini/src/main/java/co/mini/owner/command/OwnerUpdateView.java
@@ -0,0 +1,16 @@
+package co.mini.owner.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.mini.common.Action;
+
+public class OwnerUpdateView implements Action{
+
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response) {
+		
+		return null;
+	}
+
+}
