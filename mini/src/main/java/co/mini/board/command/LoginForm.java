0fb3eaa313be5ea20a02c28526cf35756c96a193
haez <hae_z@kakao.com>
2020-11-13 10:23:46 +0900

    1113 해주 최종

--- mini/src/main/java/co/mini/board/command/LoginForm.java
+++ mini/src/main/java/co/mini/board/command/LoginForm.java
@@ -2,6 +2,7 @@ package co.mini.board.command;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import co.mini.common.Action;
 
@@ -12,6 +13,8 @@ public class LoginForm implements Action {
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
 		// 메뉴 - 로그인   => 서블릿에   .do - LoginForm
+		HttpSession session = request.getSession();
+		session.invalidate();
 		
 		return "jsp/main/loginForm.jsp";
 	}
